041404901ad60ca4a06f9c956bcf4dddbb5754d8
Cameron Mackenzie <ckameron99@gmail.com>
2020-12-31 05:56:44 -0800

    added second row of info, with buttons!

--- main.py
+++ main.py
@@ -3,15 +3,60 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix import label
 from kivy.graphics import *
 
+import socket
+
+
+class API():
+    def __init__(self):
+        self.robotIP="192.168.1.254"
+        self.appIP=socket.gethostbyname(socket.gethostname())
+
+    def attemptConnect(self):
+        return False
+
+
+
 
 class UI(Screen):
     def __init__(self,**kwargs):
-        self.battery=0.0
+        self.api=API()
+        self.batteryStatus=0.0
+        self.positionStatus=(0,0)
+        self.pitchStatus=0.0
+        self.rollStatus=0.0
         super().__init__(**kwargs)
+
     def updateBattery(self,delta):
         self.battery+=delta
         self.battery=round(self.battery,3)
-        self.ids.battery.text="Battery: {}".format(self.battery)
+        self.ids.batteryStatus.text="Battery: {}".format(self.battery)
+
+    def connectUI(self):
+        connectionSuccess=self.api.attemptConnect()
+        self.ids.connectButton.text="Connection to RPi: {}".format(
+            "SUCCESS" if connectionSuccess else "FAILED"
+            )
+
+    def connectRemote(self):
+        self.ids.remoteButton.text="Remote: ON"
+
+    def connectLidar(self):
+        pass
+
+    def connectOrientation(self):
+        pass
+
+    def connectMotors(self):
+        pass
+
+    def reboot(self):
+        pass
+
+    def powerDown(self):
+        pass
+
+    def update(self):
+        pass
 
 
 class MainApp(App):
