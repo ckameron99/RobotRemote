e7bd6578d0e6a367b6c2a5e828533956c83377a1
Ankit Bhattarai <ankitbhattarai.ab@protonmail.com>
2021-01-11 15:22:29 +0000

    Speech Update

--- control.py
+++ control.py
@@ -1,9 +1,10 @@
-from deepspeech import speech_recognise
+from speech import speech_recognise
 
 
 def action():
     text = speech_recognise()
     lst_of_commands = ["left", "right", "up", "down"]
+    print("Recognised Commands")
     for command in lst_of_commands:
         if command in text:
             print(command)
